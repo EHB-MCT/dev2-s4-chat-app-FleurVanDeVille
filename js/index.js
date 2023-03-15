a3caa5d357336096237fbdcc16b60a80a48cf2ab
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2023-03-15 16:18:48 +0100

    oef bezig

--- js/index.js
+++ js/index.js
@@ -3,14 +3,25 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch("https://dev2chat.onrender.com/messages")
+            .then (function (response){
+                return response.json();
+            })
+            .then (function (messages){
+                console.log(messages);
+                const message = messages[0];
+            });
+
     },
     renderMessage(message) {
+    document.querySelector('#messageContainer').innerHTML = messages[0].message;
     }
 
-}
+};
 
 chat.init();
